157c67092136ac2c17d1a3f2f8a2f2d4a0cd1986
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-06-19 10:58:22 +0200

    Hand animations

---
+++ VR Oboarding for Roadshows/Assets/Scripts/HandAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HandAnimation : MonoBehaviour
+{
+    [SerializeField] private InputActionReference gripReference;
+    [SerializeField] private InputActionReference triggerReference;
+
+    [SerializeField] private Animator handAnimator;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float gripValue = gripReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Grip", gripValue);
+
+        float triggerValue = triggerReference.action.ReadValue<float>();
+        handAnimator.SetFloat("Trigger", triggerValue);
+    }
+}
