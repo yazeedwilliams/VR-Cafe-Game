ae3757bedf7d2466503489566f7af69ba8398cf2
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-06-21 14:27:24 +0200

    game states

--- VR Oboarding for Roadshows/Assets/Scripts/GameManager.cs
+++ VR Oboarding for Roadshows/Assets/Scripts/GameManager.cs
@@ -5,15 +5,68 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    private enum State { 
+        WaitingToStart,
+        HandAndControllerTut,
+        CountdownToStartGame, 
+        GamePlaying, 
+        GameOver
+    }
+
     [SerializeField] private TextMeshProUGUI gameOverText;
+
+    private State state;
+
+    private float waitingToStartTimer = 1f;
+    private float HandAndControllerTutTimer = 3f;
+    private float countdownToStartGameTimer = 3f;
+    private float gamePlayingTimer = 15f;
+
+    private void Awake()
+    {
+        state = State.WaitingToStart;
+    }
+
+    private void Update()
+    {
+        switch (state)
+        {
+            case State.WaitingToStart:
+                waitingToStartTimer -= Time.deltaTime;
+                if (waitingToStartTimer < 0f)
+                {
+                    state = State.HandAndControllerTut;
+                }
+                break;
+            case State.HandAndControllerTut:
+                HandAndControllerTutTimer -= Time.deltaTime;
+                if (HandAndControllerTutTimer < 0f)
+                {
+                    state = State.CountdownToStartGame;
+                }
+                break;
+            case State.CountdownToStartGame:
+                countdownToStartGameTimer -= Time.deltaTime;
+                if (countdownToStartGameTimer < 0f)
+                {
+                    state = State.GamePlaying;
+                }
+                break;
+            case State.GamePlaying:
+                gamePlayingTimer -= Time.deltaTime;
+                if (gamePlayingTimer < 0f)
+                {
+                    state = State.GameOver;
+                }
+                break;
+            case State.GameOver:
+                break;
+        }
+        Debug.Log(state);
+    }
 
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(false);
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        
     }
 }
