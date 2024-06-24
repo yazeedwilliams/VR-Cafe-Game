d56b343fc7cc236a715aa470d80dfdc95a4bafcb
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-06-24 14:41:04 +0200

    Countdown Timer

---
+++ VR Oboarding for Roadshows/Assets/Scripts/GameStartTimer.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameStartTimer : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI gameStartTimerText;
+    [SerializeField] private float startTime = 3f;
+    [SerializeField] private UnityEvent onGameStart;
+
+    private float currentTime;
+
+    private void OnEnable()
+    {
+        ResetTimer();
+        StartCoroutine(Countdown());
+    }
+
+    private void ResetTimer()
+    {
+        currentTime = startTime;
+        gameStartTimerText.text = Mathf.Ceil(currentTime).ToString();
+    }
+
+    private IEnumerator Countdown()
+    {
+        while (currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            gameStartTimerText.text = Mathf.Ceil(currentTime).ToString();
+            yield return null;
+        }
+        TimerEnded();
+    }
+
+    private void TimerEnded()
+    { 
+        gameStartTimerText.text = "Go!";
+        onGameStart.Invoke();
+    }
+}
