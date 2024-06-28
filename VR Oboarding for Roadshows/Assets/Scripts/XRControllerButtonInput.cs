3fd0c612a468df8abc741bd5969912a5b9a2099c
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-06-28 16:14:00 +0200

    Button pressed fixed but not yet completed

--- VR Oboarding for Roadshows/Assets/Scripts/XRControllerButtonInput.cs
+++ VR Oboarding for Roadshows/Assets/Scripts/XRControllerButtonInput.cs
@@ -4,15 +4,27 @@ using UnityEngine.InputSystem;
 public class XRControllerButtonInput : MonoBehaviour
 {
     [SerializeField] private InputActionReference triggerButtonAction;
+    [SerializeField] private InputActionReference gripButtonAction; 
+    [SerializeField] private InputActionReference primaryButtonAction;
+    [SerializeField] private InputActionReference secondaryButtonAction; 
+    [SerializeField] private InputActionReference analogueButtonAction;
 
     private void OnEnable()
     {
         triggerButtonAction.action.performed += OnButtonPress;
+        gripButtonAction.action.performed += OnButtonPress;
+        primaryButtonAction.action.performed += OnButtonPress;
+        secondaryButtonAction.action.performed += OnButtonPress;
+        analogueButtonAction.action.performed += OnButtonPress;
     }
 
     private void OnDisable()
     {
         triggerButtonAction.action.performed -= OnButtonPress;
+        gripButtonAction.action.performed -= OnButtonPress;
+        primaryButtonAction.action.performed -= OnButtonPress;
+        secondaryButtonAction.action.performed -= OnButtonPress;
+        analogueButtonAction.action.performed -= OnButtonPress;
     }
 
     private void OnButtonPress(InputAction.CallbackContext context)
