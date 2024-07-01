ddf2d43c11f0261b304ce810f9683fb8246c8ab3
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-07-01 17:58:16 +0200

    tutorial buttons almost finished

--- VR Oboarding for Roadshows/Assets/Scripts/TutorialManager.cs
+++ VR Oboarding for Roadshows/Assets/Scripts/TutorialManager.cs
@@ -2,10 +2,61 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TutorialManager : MonoBehaviour
 {
-    [SerializeField] private GameObject[] targetObject;
-    [SerializeField] private float activeTime = 5f;
+    [SerializeField] private GameObject[] instructionsText;
+    [SerializeField] private XRControllerButtonInput buttonInput;
 
+    private void Start()
+    {
+        if (instructionsText.Length > 0)
+        {
+            instructionsText[0].SetActive(true);
+        }
+
+        // Subscribe to button press events
+        if (buttonInput != null)
+        {
+            buttonInput.OnButtonPressEvent += HandleButtonPress;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (buttonInput != null)
+        {
+            buttonInput.OnButtonPressEvent -= HandleButtonPress;
+        }
+    }
+
+    private void HandleButtonPress(InputActionReference actionReference)
+    {
+        if (actionReference == buttonInput.leftTriggerButtonAction || actionReference == buttonInput.rightTriggerButtonAction)
+        {
+            instructionsText[0].SetActive(false);
+            instructionsText[1].SetActive(true);
+        }
+        else if (actionReference == buttonInput.leftGripButtonAction || actionReference == buttonInput.rightGripButtonAction)
+        {
+            instructionsText[1].SetActive(false);
+            instructionsText[2].SetActive(true);
+        }
+        else if (actionReference == buttonInput.leftSecondaryButtonAction || actionReference == buttonInput.rightSecondaryButtonAction)
+        {
+            instructionsText[2].SetActive(false);
+            instructionsText[3].SetActive(true);
+        }
+        else if (actionReference == buttonInput.leftPrimaryButtonAction || actionReference == buttonInput.rightPrimaryButtonAction)
+        {
+            instructionsText[3].SetActive(false);
+            instructionsText[4].SetActive(true);
+        }
+        else if (actionReference == buttonInput.leftAnalogueButtonAction || actionReference == buttonInput.rightAnalogueButtonAction)
+        {
+            instructionsText[4].SetActive(false);
+            instructionsText[5].SetActive(true);
+        }
+    }
 }
--- VR Oboarding for Roadshows/Assets/Scripts/XRControllerButtonInput.cs
+++ VR Oboarding for Roadshows/Assets/Scripts/XRControllerButtonInput.cs
@@ -1,23 +1,26 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 public class XRControllerButtonInput : MonoBehaviour
 {
+    public event Action<InputActionReference> OnButtonPressEvent;
+
     [Header("Left Controller Actions")]
-    [SerializeField] private InputActionReference leftTriggerButtonAction;
-    [SerializeField] private InputActionReference leftGripButtonAction;
-    [SerializeField] private InputActionReference leftPrimaryButtonAction;
-    [SerializeField] private InputActionReference leftSecondaryButtonAction;
-    [SerializeField] private InputActionReference leftAnalogueButtonAction;
+    public InputActionReference leftTriggerButtonAction;
+    public InputActionReference leftGripButtonAction;
+    public InputActionReference leftPrimaryButtonAction;
+    public InputActionReference leftSecondaryButtonAction;
+    public InputActionReference leftAnalogueButtonAction;
 
     [Header("Right Controller Actions")]
-    [SerializeField] private InputActionReference rightTriggerButtonAction;
-    [SerializeField] private InputActionReference rightGripButtonAction;
-    [SerializeField] private InputActionReference rightPrimaryButtonAction;
-    [SerializeField] private InputActionReference rightSecondaryButtonAction;
-    [SerializeField] private InputActionReference rightAnalogueButtonAction;
+    public InputActionReference rightTriggerButtonAction;
+    public InputActionReference rightGripButtonAction;
+    public InputActionReference rightPrimaryButtonAction;
+    public InputActionReference rightSecondaryButtonAction;
+    public InputActionReference rightAnalogueButtonAction;
 
     private Dictionary<InputActionReference, Action<InputAction.CallbackContext>> leftActionHandlers;
     private Dictionary<InputActionReference, Action<InputAction.CallbackContext>> rightActionHandlers;
@@ -26,20 +29,20 @@ public class XRControllerButtonInput : MonoBehaviour
     {
         leftActionHandlers = new Dictionary<InputActionReference, Action<InputAction.CallbackContext>>
         {
-            { leftTriggerButtonAction, context => OnButtonPress(context, "Left Trigger Pressed") },
-            { leftGripButtonAction, context => OnButtonPress(context, "Left Grip Pressed") },
-            { leftPrimaryButtonAction, context => OnButtonPress(context, "Left Primary Pressed") },
-            { leftSecondaryButtonAction, context => OnButtonPress(context, "Left Secondary Pressed") },
-            { leftAnalogueButtonAction, context => OnButtonPress(context, "Left Analogue Pressed") }
+            { leftTriggerButtonAction, context => OnButtonPress(context, leftTriggerButtonAction) },
+            { leftGripButtonAction, context => OnButtonPress(context, leftGripButtonAction) },
+            { leftPrimaryButtonAction, context => OnButtonPress(context, leftPrimaryButtonAction) },
+            { leftSecondaryButtonAction, context => OnButtonPress(context, leftSecondaryButtonAction) },
+            { leftAnalogueButtonAction, context => OnButtonPress(context, leftAnalogueButtonAction) }
         };
 
         rightActionHandlers = new Dictionary<InputActionReference, Action<InputAction.CallbackContext>>
         {
-            { rightTriggerButtonAction, context => OnButtonPress(context, "Right Trigger Pressed") },
-            { rightGripButtonAction, context => OnButtonPress(context, "Right Grip Pressed") },
-            { rightPrimaryButtonAction, context => OnButtonPress(context, "Right Primary Pressed") },
-            { rightSecondaryButtonAction, context => OnButtonPress(context, "Right Secondary Pressed") },
-            { rightAnalogueButtonAction, context => OnButtonPress(context, "Right Analogue Pressed") }
+            { rightTriggerButtonAction, context => OnButtonPress(context, rightTriggerButtonAction) },
+            { rightGripButtonAction, context => OnButtonPress(context, rightGripButtonAction) },
+            { rightPrimaryButtonAction, context => OnButtonPress(context, rightPrimaryButtonAction) },
+            { rightSecondaryButtonAction, context => OnButtonPress(context, rightSecondaryButtonAction) },
+            { rightAnalogueButtonAction, context => OnButtonPress(context, rightAnalogueButtonAction) }
         };
     }
 
@@ -69,11 +72,35 @@ public class XRControllerButtonInput : MonoBehaviour
         }
     }
 
-    private void OnButtonPress(InputAction.CallbackContext context, string message)
+    private void OnButtonPress(InputAction.CallbackContext context, InputActionReference actionReference)
     {
         if (context.performed)
         {
-            Debug.Log(message);
+            if (actionReference == leftTriggerButtonAction || actionReference == rightTriggerButtonAction)
+            {
+                Debug.Log("Trigger");
+                OnButtonPressEvent?.Invoke(actionReference);
+            }
+            else if (actionReference == leftGripButtonAction || actionReference == rightGripButtonAction)
+            {
+                Debug.Log("Grip");
+                OnButtonPressEvent?.Invoke(actionReference);
+            }
+            else if (actionReference == leftPrimaryButtonAction || actionReference == rightPrimaryButtonAction)
+            {
+                Debug.Log("Primary");
+                OnButtonPressEvent?.Invoke(actionReference);
+            }
+            else if (actionReference == leftSecondaryButtonAction || actionReference == rightSecondaryButtonAction)
+            {
+                Debug.Log("Secondary");
+                OnButtonPressEvent?.Invoke(actionReference);
+            }
+            else if (actionReference == leftAnalogueButtonAction || actionReference == rightAnalogueButtonAction)
+            {
+                Debug.Log("Analogue");
+                OnButtonPressEvent?.Invoke(actionReference);
+            }
         }
     }
 }
