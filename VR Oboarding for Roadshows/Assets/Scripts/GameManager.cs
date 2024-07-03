a5a66de83010ebaae9da439a117e1e3502c4959c
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-07-03 13:46:03 +0200

    Level one flow

--- VR Oboarding for Roadshows/Assets/Scripts/GameManager.cs
+++ VR Oboarding for Roadshows/Assets/Scripts/GameManager.cs
@@ -5,24 +5,28 @@ using UnityEngine.Events;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject introBackground;
+    [SerializeField] private GameObject rayBackground;
+    [SerializeField] private GameObject selectAndMoveBackground;
     [SerializeField] private TextMeshProUGUI gameTimerText;
     [SerializeField] private UnityEvent onGameEnd;
 
     private ScoreCounter scoreCount;
 
+    private float timeDelay = 4f;
     private float startTime = 20f;
     private float currentTime;
 
     private void Start()
     {
         scoreCount = FindAnyObjectByType<ScoreCounter>();
+        StartCoroutine(Deactivate());
     }
 
     public void StartGame()
     {
         ResetTimer();
         StartCoroutine(GameTimeCountdown());
-        //StartCoroutine(GameScore());
     }
 
     private void ResetTimer()
@@ -40,7 +44,6 @@ public class GameManager : MonoBehaviour
             if (scoreCount.GetScore() == 2)
             {
                 GameOver();
-                //yield return null;
             }
             yield return null;
         }
@@ -61,5 +64,15 @@ public class GameManager : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
+    }
+
+    private IEnumerator Deactivate()
+    {
+        yield return new WaitForSeconds(timeDelay);
+        introBackground.SetActive(false);
+        rayBackground.SetActive(true);
+        yield return new WaitForSeconds(timeDelay);
+        rayBackground.SetActive(false);
+        selectAndMoveBackground.SetActive(true);
     }
 }
