b815737ca9d51ed33799a79600a8e911821a2ba1
Yazeed Williams <yazeedwilliams13@gmail.com>
2024-07-09 16:12:32 +0200

    Increased the time

--- VR Oboarding for Roadshows/Assets/Scripts/GameManager.cs
+++ VR Oboarding for Roadshows/Assets/Scripts/GameManager.cs
@@ -20,7 +20,7 @@ public class GameManager : MonoBehaviour
     private ScoreCounter scoreCount;
 
     private float timeDelay = 13f;
-    private float startTime = 20f;
+    private float startTime = 45f;
     private float currentTime;
 
     private void Start()
